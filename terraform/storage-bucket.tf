781e20376b3e703d2e873f95a455157bd660b24e
Sergey Makeev <mac.798@mail.ru>
2018-08-05 13:30:15 +0300

    Another fix for travis tests

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,9 +1,3 @@
-provider "google" {
-  version = "1.4.0"
-  project = "${var.project}"
-  region  = "${var.region}"
-}
-
 module "storage-bucket" {
   source  = "SweetOps/storage-bucket/google"
   version = "0.1.1"
