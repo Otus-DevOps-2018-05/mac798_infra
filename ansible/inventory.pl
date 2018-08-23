8b2cd013db7fa167db420bd0b3d196900f55e9e8
Sergey Makeev <mac.798@mail.ru>
2018-08-24 00:02:28 +0300

    sync commit

--- ansible/inventory.pl
+++ ansible/inventory.pl
@@ -3,12 +3,25 @@ use JSON;
 my %output_data = ( all => {children =>["app", "db", "ungrouped"]}, app => [], db => [], ungrouped =>[], _meta =>  {hostvars => {}} );
 
 my @lines = split( /[\r\n]+/, `gcloud compute instances list` );
- shift @lines;
- 
+my $header = shift @lines;
+my @fields = split /\s+/, $header;
+
+my $pos = 0;
+my %offsets = ();
+
+foreach my $field (@fields) {
+    my $newpos = index $header, $field, $pos;
+    $offsets{$field} = $newpos;
+}
+
 foreach my $record (@lines) {
-  my @fields = split /\s+/, $record;
-  $name = $fields[0];
-  $ext_ip = $fields[$#fields-1];
+    my %splittedRecord = ();
+    foreach my $field (@fields) {
+        my $sub = substr $record, $offsets{$field};
+        my($val) = ( $sub =~ m/(\S*)(\s+|$)/);
+        $splittedRecord{$field} = $val;
+    }
+  $name = $splittedRecord{NAME};;
   if ($name =~ m/-app/) {
     push @{$output_data{app}}, $name;
   } elsif ($name =~ m/-db/) {
@@ -16,7 +29,13 @@ foreach my $record (@lines) {
   } else {
     push @{$output_data{ungrouped}}, $name;
   }
-  %{$output_data{_meta}{hostvars}{$name}} = ( ansible_host => $ext_ip );
+  %{$output_data{_meta}{hostvars}{$name}} =
+    (
+      ansible_host => $splittedRecord{EXTERNAL_IP},
+      internal_ip => $splittedRecord{INTERNAL_IP},
+      zone => $splittedRecord{ZONE},
+      mtype => $splittedRecord{MACHINE_TYPE}
+    );
 }
 
 print JSON->new->utf8->pretty->encode(\%output_data),"\n";
