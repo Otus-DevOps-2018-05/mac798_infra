25c36ab76c480281d19be08c8993d1426665dfb9
Sergey Makeev <makeev@vteleport.ru>
2018-08-27 17:16:40 +0300

    Final commit (hopefully)

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,7 +1,7 @@
 module "storage-bucket" {
   source  = "SweetOps/storage-bucket/google"
   version = "0.1.1"
-  name    = ["terraform-storage-bucket", "terraform-storage-stage"]
+  name    = ["terraform-storage-bucket-2", "terraform-storage-stage-2"]
 }
 
 output storage-bucket_url {
