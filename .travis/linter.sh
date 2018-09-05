c2e87f37b19135a1f5e6f50b77cb1b32a75aff9f
Sergey Makeev <mac.798@mail.ru>
2018-09-06 00:05:41 +0300

    Change linter parameters

--- .travis/linter.sh
+++ .travis/linter.sh
@@ -5,11 +5,11 @@ for pk_tpl in `find ./packer -name \\*.json -and -not -name variables.json`; do
   echo "Validating $pk_tpl"
   if grep '"playbook_file": *"ansible/' "$pk_tpl" ; then
     val_file="$pk_tpl"
-    var_file=packer/variables.json
+    var_file=packer/variables.json.example
   else
     cd "$wd/packer"
     val_file="../$pk_tpl"
-    var_file=variables.json
+    var_file=variables.json.examples
   fi
   packer validate -var-file=$var_file "$val_file" || exit 1
   cd "$wd"
