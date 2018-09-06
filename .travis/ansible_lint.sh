e3177f13e8f14b6637b81cc17b94ead44976eb61
Sergey Makeev <mac.798@mail.ru>
2018-09-06 22:30:51 +0300

    yet another travis fix

--- .travis/ansible_lint.sh
+++ .travis/ansible_lint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 ANSIBLE_LINT=`which ansible-lint`
 if [ "${ANSIBLE_LINT}" = "" ]; then
+  sudo easy_install ansible-lint
   ANSIBLE_LINT=`find / -type f -executable -name ansible-lint 2>/dev/null|head -1`
-  [ "${ANSIBLE_LINT}" = "" ] && exit 1
+  if [ "${ANSIBLE_LINT}" = "" ]; then
+    echo "ansible-lint not found, exiting"
+    exit 1
+  fi
 fi
 echo Ansible lint "(${ANSIBLE_LINT})"
 for ansible_yml in ansible/playbooks/*.yml ; do
