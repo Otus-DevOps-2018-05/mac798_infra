a35c5a343f05932b9647be896b8f07a2e0a9d084
Sergey Makeev <mac.798@mail.ru>
2018-09-06 09:19:05 +0300

    travis fix :)

--- .travis/linter.sh
+++ .travis/linter.sh
@@ -1,15 +1,24 @@
 #!/bin/#!/bin/sh
 wd=`pwd`
 
+echo "Checking ansible installation"
+which ansible-playbook || pip install ansible || exit 1
+
+if [ -f packer/variables.json ]; then
+  var_file_base=variables.json
+else
+  var_file_base=variables.json.example
+fi
+
 for pk_tpl in `find ./packer -name \\*.json -and -not -name variables.json`; do
   echo "Validating $pk_tpl"
   if grep '"playbook_file": *"ansible/' "$pk_tpl" ; then
     val_file="$pk_tpl"
-    var_file=packer/variables.json.example
+    var_file=packer/$var_file_base
   else
     cd "$wd/packer"
     val_file="../$pk_tpl"
-    var_file=variables.json.example
+    var_file=$var_file_base
   fi
   echo   packer validate -var-file=$var_file "$val_file"
   packer validate -var-file=$var_file "$val_file" || exit 1
