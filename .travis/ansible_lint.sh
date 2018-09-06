3ca3eca48e6ffe5d9af723f731724c740b8f02a9
Sergey Makeev <mac.798@mail.ru>
2018-09-06 22:42:33 +0300

    yet another travis fix

--- .travis/ansible_lint.sh
+++ .travis/ansible_lint.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 ANSIBLE_LINT=`which ansible-lint`
 if [ "${ANSIBLE_LINT}" = "" ]; then
-  sudo easy_install ansible-lint
   ANSIBLE_LINT=`find / -type f -executable -name ansible-lint 2>/dev/null|head -1`
   if [ "${ANSIBLE_LINT}" = "" ]; then
     echo "ansible-lint not found, exiting"
-    exit 1
+    //exit 1
   fi
 fi
 echo Ansible lint "(${ANSIBLE_LINT})"
 for ansible_yml in ansible/playbooks/*.yml ; do
   echo Run linter for $ansible_yml
-  ${ANSIBLE_LINT} $ansible_yml || exit 1
+  docker exec hw-test ansible-lint $ansible_yml || exit 1
 done
