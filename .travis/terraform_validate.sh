5779f20c53a95480322eb2ac12fe96140e4d8ced
Sergey Makeev <mac.798@mail.ru>
2018-09-10 09:39:46 +0300

    travis fix (terrraform validation part -- remove cloud backend storage)

--- .travis/terraform_validate.sh
+++ .travis/terraform_validate.sh
@@ -2,5 +2,5 @@
 TERRAFORM_ENV="stage prod"
 
 for env in $TERRAFORM_ENV; do
-  docker exec hw-test bash -c "cd ./terraform/$env && echo terraform validate `pwd` && terraform init && terraform validate && echo Run linter on terraform files in `pwd` && tflint *.tf"
+  docker exec hw-test bash -c "cd ./terraform/$env && echo terraform validate `pwd` && mv backend.tf backend.tf~ && terraform init && terraform validate && echo Run linter on terraform files in `pwd` && tflint *.tf backend.tf~" || exit 1
 done
