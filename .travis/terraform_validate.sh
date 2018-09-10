1b60d2dd5ee7af5434f4d379add5c8db68966b42
Sergey Makeev <mac.798@mail.ru>
2018-09-10 09:44:03 +0300

    Travis fix

--- .travis/terraform_validate.sh
+++ .travis/terraform_validate.sh
@@ -2,5 +2,5 @@
 TERRAFORM_ENV="stage prod"
 
 for env in $TERRAFORM_ENV; do
-  docker exec hw-test bash -c "cd ./terraform/$env && echo terraform validate `pwd` && mv backend.tf backend.tf~ && terraform init && terraform validate && echo Run linter on terraform files in `pwd` && tflint *.tf backend.tf~" || exit 1
+  docker exec hw-test bash -c "cd ./terraform/$env && echo terraform validate `pwd` && mv backend.tf backend.tf~ && cp terraform.tfvars.example terraform.tfvars && terraform init && terraform validate && echo Run linter on terraform files in `pwd` && tflint *.tf backend.tf~" || exit 1
 done
